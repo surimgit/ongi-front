e283b75b56d0503b7583c4bf63e7eb86f899f394
eun23 <127043779+eun23@users.noreply.github.com>
2025-04-11 16:41:42 +0900

    feat: 도우미 게시판 UI 및 지역 선택 드롭다운 구현
    - 도우미 게시판 기본 UI 레이아웃 구성
    - 시/도 및 시/군/구 드롭다운 구현
    - 도우미 리스트 더미 데이터 출력 구현
    - 버튼 및 테이블 디자인 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { Routes, Route } from 'react-router';
 import Layout from './layouts/Layout';
 import HomePage from './views/HomePage';
 import './App.css';
+import { NEEDHELPER_PATH } from './constants';
+import NeedHelper from './views/NeedHelper';
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<HomePage />} />
+
+        <Route path={NEEDHELPER_PATH}>
+          <Route index element={<NeedHelper />} />
+        </Route>
       </Route>
     </Routes>
   );
---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+// variable: 상대 path 상수 //
+export const ROOT_PATH = '/';
+export const NEEDHELPER_PATH = 'needHelper';
+
+// variable: 절대 path 상수 //
+export const ROOT_ABSOLUTE_PATH = ROOT_PATH;
+export const NEEDHELPER_ABSOLUTE_PATH = `${ROOT_PATH}${NEEDHELPER_PATH}`;
