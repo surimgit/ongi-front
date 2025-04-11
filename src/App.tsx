5d394018471d72d40c11b5d493062812e4fbe516
YooKyuBong <58059368+ykb-i@users.noreply.github.com>
2025-04-11 14:27:49 +0900

    product 등록 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { Routes, Route } from 'react-router';
 import Layout from './layouts/Layout';
 import HomePage from './views/HomePage';
 import './App.css';
+import { GROUP_PURCHASE_PATH } from './constants';
+import GroupPurchaseMain from './views/GroupPurchase';
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<HomePage />} />
+
+        <Route path={GROUP_PURCHASE_PATH}>
+          <Route index element={<GroupPurchaseMain/>}/>
+        </Route>
       </Route>
     </Routes>
   );
---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+// variable: 상대 path 변수 //
+export const ROOT_PATH = '/';
+export const GROUP_PURCHASE_PATH = 'product';
+export const PRODUCT_VIEW_PATH = ':productNumber';
+
+// variable: 절대 path 상수 //
+export const ROOT_ABSOLUTE_PATH = ROOT_PATH;
+export const GROUP_PURCHASE_ABSOLUTE_PATH = `${ROOT_PATH}/${GROUP_PURCHASE_PATH}`
