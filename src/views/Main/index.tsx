d2865eb73016aebc7b8ea10e9eb85975ede3cc3f
skrudPNK <akdldkdlel13@gmail.com>
2025-04-17 12:35:15 +0900

    feat: main page 제작중 / perf: 회원가입시 필요조건 name>nickname으로 변경

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,22 +1,14 @@
-import React from "react";
-
+import React, { useState } from "react";
+import AdSlider from "src/components/AdSlider";
+import GroupBuyingSlider from "src/components/GroupByingSlider";
 import './style.css';
-import adImage from 'src/assets/images/ad-sample-image.png';
 
 export default function Main() {
 
-    // render: Main 화면 컴포넌트 렌더링 //
     return (
         <div id="main-wrapper">
-            <div className="ad-container">
-                <img className="ad-image" src={adImage}/>
-                <div className="ad-text">
-                    <div className="ad-content">완전 완전 대박 좋은<br />비타민 C, D</div>
-                    <div className="ad-price">34,000원</div>
-                </div>
-                <div className="ad-next"></div>
-            </div>
-            <div className="group-bying-container"></div>
+            <AdSlider />
+            <GroupBuyingSlider />            
             <div className="helper-container"></div>
             <div className="community-container"></div>
             <div className="hot-community-container"></div>
